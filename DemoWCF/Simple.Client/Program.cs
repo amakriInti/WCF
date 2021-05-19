5c6c14370b5f2298e3e77b785ff975591a6eb288
Ali MAKRI <microsoft@makri.fr>
2021-05-19 15:47:37 +0200

    15:47

---
+++ DemoWCF/Simple.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Simple.Client.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var s = Console.ReadLine();
+            Service1Client service = new Service1Client();
+
+            var resultat = service.Minuscule(s);
+            Console.WriteLine(resultat);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DemoWCF/Simple.Server/Service1.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Simple.Server
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class Service1 : IService1
+    {
+        public string Majuscule(string value)
+        {
+            return value.ToUpper();
+        }
+
+        public string Minuscule(string value)
+        {
+            return value.ToLower();
+        }
+    }
+}
